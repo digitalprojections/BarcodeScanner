24ac96eab887b0b7026d9a69e5c29cce014ca462
digitalprojections <fuzalov@gmail.com>
2021-12-11 21:33:19 +0900

    adding database

---
+++ BarcodeScanner/DATABASE/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BarcodeScanner.DATABASE
+{
+    public class Categories
+    {
+        public int CategoriesID { get; set; }
+
+        public string Name { get; set; }
+
+        public int ProductsID { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ BarcodeScanner/DATABASE/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarcodeScanner.DATABASE
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+        public string BarCode { get; set; }
+        public string Description { get; set; }
+    
+        public int CategoriesID { get; set; }
+        public virtual Categories Categories { get; set; }
+}
+}
